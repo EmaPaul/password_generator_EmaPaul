21c2c96e04cda30cf273e6bb172976217abbe969
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-01-22 16:20:08 -0500

    archivos_de_react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GeneratorPassword from './generadorContraseñas/GeneratorPassword';
+
+function App() {
+  return (
+    <div className="App">
+     <GeneratorPassword/>
+    </div>
+  );
+}
+
+export default App;
